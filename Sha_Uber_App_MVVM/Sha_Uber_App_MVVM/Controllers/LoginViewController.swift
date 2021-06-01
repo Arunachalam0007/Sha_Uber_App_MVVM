75b76affbdef4c467ff47539f236c45dfdcea5de
Arunachalam0007 <arunsha0007@gmail.com>
2021-06-01 11:22:35 +0530

    Added Sample Code with Folders

---
+++ Sha_Uber_App_MVVM/Sha_Uber_App_MVVM/Controllers/LoginViewController.swift
@@ -0,0 +1,17 @@
+//
+//  LoginViewController.swift
+//  Sha_Uber_App_MVVM
+//
+//  Created by ArunSha on 01/06/21.
+//
+
+import UIKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = .systemTeal
+    }
+
+}
